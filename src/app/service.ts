b54ccfc394d33fc14ba1af05d0ae1319c2a15506
Eric Borbely <eric.borbely@gmail.com>
2018-05-03 21:28:52 -0400

    Cache the book to localstorage

--- src/app/service.ts
+++ src/app/service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import "rxjs/add/operator/do";
+
 
 @Injectable()
 export class DataService {
-    constructor(private http: HttpClient) {}
-    path = 'assets/mydailybread.json';
 
-    get() {
-        return this.http.get(this.path);
+    constructor(private http: HttpClient) {}
+
+    get() : Observable<Object> {      
+        return this.getFromCache() ||
+                this.getFromServer();
+    }
+
+    private getFromCache() : Observable<Object> {
+        var json = localStorage.getItem('mdb');
+
+        return json
+            ? Observable.of(JSON.parse(json))
+            : null;
+    }
+
+    private getFromServer() : Observable<Object> {
+        return this.http
+            .get('assets/mydailybread.json')
+            .do(books => localStorage.setItem('mdb', JSON.stringify(books)));
     }
 
 }
